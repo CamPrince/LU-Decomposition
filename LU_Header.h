5efe89b68bcd80b94fb05bd34b69e13f82e58712
Cam Prince <camprince2001@gmail.com>
2022-11-07 11:41:32 +0000

    Header
    Includes LU_decomp and read_matrix

---
+++ LU_Header.h
@@ -0,0 +1,8 @@
+#ifndef LU_HEADER_H_INCLUDED
+#define LU_HEADER_H_INCLUDED
+
+extern void LU_Decomp(float **input, float **u, float **l, int n);
+extern void read_matrix(float **arr,int row, int col);
+
+
+#endif // LU_HEADER_H_INCLUDED
